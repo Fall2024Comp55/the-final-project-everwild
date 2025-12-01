1918ced7d0a94e72a6d2160081af499785bec742
Vanessa Chen <v_chen20@u.pacific.edu>
2025-12-01 15:35:51 -0800

    try to commmit and push what i did.

---
+++ src/FontStyle.java
@@ -0,0 +1,7 @@
+import java.awt.Font;
+
+public class FontStyle {
+    public static final Font STAT_FONT = new Font("Arial Rounded MT Bold", Font.PLAIN, 22);
+    public static final Font DESC_FONT = new Font("Arial Rounded MT Bold", Font.PLAIN, 18);
+    public static final Font TITLE_FONT = new Font("Arial Rounded MT Bold", Font.BOLD, 24);
+}
